645594887de97326a070b1148f2350e2dd67b4dc
HamHarry <hamza_2641@hotmail.com>
2022-05-11 23:37:31 +0700

    จบ

--- base.js
+++ base.js
@@ -14,65 +14,73 @@ const con = mysql.createPool({
 })
 
 var obj = {}
-var obj2 = {}
-var obj3 = {}
+var obj1 = {}
 app.set('view engine','ejs')
 app.use(express.static('ALLCSS'))
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.get("/",(req,res)=>{
+    res.render('index')
+})
+
 app.get("/profile.ejs",(req,res)=>{
     res.render('profile')
+})
+
+app.get("/addall.ejs",(req,res)=>{
+    res.render('addall')
 })
 
 app.get("/add.ejs",(req,res)=>{
     res.render('add')
 })
 
-app.get('/',(req,res)=>{
+app.get("/add1.ejs",(req,res)=>{
+    res.render('add1')
+})
+
+app.get("/add2.ejs",(req,res)=>{
+    res.render('add2')
+})
+
+app.get('/March.ejs',(req,res)=>{
     con.getConnection((err,connection)=>{
         if(err) throw err
         console.log("connected id: ",connection.threadId)
-
         connection.query('SELECT * FROM march16',(err,rows)=>{
-        
         connection.release();
             if(err){
                 console.log(err)
             }
             else{
                 obj = {Error: err,march16: rows}
-                res.render('index',obj)
+                res.render('March',obj)
             }
         })
     })
 })
 
-app.get('/',(req,res)=>{
+app.get('/April.ejs',(req,res)=>{
     con.getConnection((err,connection)=>{
         if(err) throw err
         console.log("connected id: ",connection.threadId)
-        connection.query('SELECT * FROM march16',(err,rows)=>{
+        connection.query('SELECT * FROM april1',(err,rows)=>{
         connection.release();
             if(err){
                 console.log(err)
             }
             else{
-                obj = {Error: err,march16: rows}
-                res.render('index',obj)
-            }
-        })
-
-        connection.query('SELECT * FROM april1',(err,rows)=>{
-        connection.release();
-            if(err){
-                 console.log(err)
-            }
-            else{
                 obj = {Error: err,april1: rows}
-                res.render('index',obj2)
+                res.render('April',obj)
             }
         })
-         
+    })
+})
+
+app.get('/April2.ejs',(req,res)=>{
+    con.getConnection((err,connection)=>{
+        if(err) throw err
+        console.log("connected id: ",connection.threadId)
         connection.query('SELECT * FROM april16',(err,rows)=>{
         connection.release();
             if(err){
@@ -80,9 +88,66 @@ app.get('/',(req,res)=>{
             }
             else{
                 obj = {Error: err,april16: rows}
-                res.render('index',obj3)
+                res.render('April2',obj)
             }
         })
+    })
+})
+
+app.post('/add.ejs',(req, res) => {
+    con.getConnection((err, connection) => { 
+        if(err) throw err
+            const params = req.body
+
+                con.getConnection((err, connection) => {
+                            connection.query(`INSERT INTO march16 SET ?`, params, (err, rows) => {
+                                connection.release()
+                                if(!err){
+                                    obj1 = {Error:err, mesg : `Success Complete`}
+                                    res.render('add', obj1)
+                                }else {
+                                    console.log(err)
+                                    }
+                                })
+                })
+    })
+})
+
+app.post('/add1.ejs',(req, res) => {
+    con.getConnection((err, connection) => { 
+        if(err) throw err
+            const params = req.body
+
+                con.getConnection((err, connection) => {
+                            connection.query(`INSERT INTO april1 SET ?`, params, (err, rows) => {
+                                connection.release()
+                                if(!err){
+                                    obj1 = {Error:err, mesg : `Success Complete`}
+                                    res.render('add1', obj1)
+                                }else {
+                                    console.log(err)
+                                    }
+                                })
+                })
+    })
+})
+
+app.post('/add2.ejs',(req, res) => {
+    con.getConnection((err, connection) => { 
+        if(err) throw err
+            const params = req.body
+
+                con.getConnection((err, connection) => {
+                            connection.query(`INSERT INTO april16 SET ?`, params, (err, rows) => {
+                                connection.release()
+                                if(!err){
+                                    obj1 = {Error:err, mesg : `Success Complete`}
+                                    res.render('add2', obj1)
+                                }else {
+                                    console.log(err)
+                                    }
+                                })
+                })
     })
 })
 
